dc4bd65bf32fb0c51fa4ce5874e375537a2791ff
mrshos <140654273+mrshos@users.noreply.github.com>
2023-07-26 16:28:49 -0400

    SQIDL7200

---
+++ story_content/64p7bQ9aMmi_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.77.30587.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.422%0D%0AJournaling%20or%20blogging%E2%80%94but%20I%20don%E2%80%99t%20want%20to%20interact%0D%0A%0D%0A"}')
